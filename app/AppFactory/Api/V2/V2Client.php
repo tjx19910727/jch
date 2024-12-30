f4fcfee141a45e5a886e2591ad6d11548d5a57d6
Ted <272897521@qq.com>
2024-12-30 12:05:32 +0800

    调整对外API接口，调整后台导入商品Excel，修复抽奖活动BUG，增加导入商品验证器

--- app/AppFactory/Api/V2/V2Client.php
+++ app/AppFactory/Api/V2/V2Client.php
@@ -33,6 +33,7 @@ use app\AppFactory\Kernel\Traits\SaleOrders\SaleHotelTrait;
 use app\AppFactory\Kernel\Traits\SaleOrders\SaleOrdersDailyCountTrait;
 use app\AppFactory\Kernel\Traits\SaleOrders\SaleOrdersRevenueTrait;
 use app\AppFactory\Kernel\Traits\SaleOrders\SaleOrdersTrait;
+use app\AppFactory\Kernel\Traits\Strategy\StrategyIncomeTrait;
 use think\db\exception\DataNotFoundException;
 use think\db\exception\DbException;
 use think\db\exception\ModelNotFoundException;
@@ -44,6 +45,7 @@ class V2Client extends V2BaseClient
     use EarthCountriesTrait, EarthCitiesTrait, EarthRegionsTrait, EarthAreaTrait, EarthStatesTrait;
     use SaleOrdersDailyCountTrait, SaleOrdersTrait, SaleHotelTrait, SaleOrdersRevenueTrait;
     use ActivityPickTrait,ActivityPickCodeTrait,ActivityCouponTrait,ActivityCouponUsedTrait;
+    use StrategyIncomeTrait;
     use ApiAdvanceTrait, ApiCallbackTrait;
     use AfterOrderPaymentTrait;
     use GoodsTrait,GoodsCategoryTrait;
@@ -71,7 +73,7 @@ class V2Client extends V2BaseClient
             $where[] = ['status', '<>', 2];
             $data = $this->getMachineChannelList($where,$this->params['pageNum'], $field, 'stock desc');
             $data = $data->each(function ($item) {
-                $goods = $this->getGoodsFind(['g_id' => $item['product_id']],'pic,banner,sku2,`desc`,details_pic,gc_id,gc_name');
+                $goods = $this->getGoodsFind(['g_id' => $item['product_id']],'pic,banner,sku2,`desc`,details_pic,gc_id,gc_name,length,width,height');
                 $item['pic'] = $goods['pic'] ?? '';
                 $item['details_pic'] = $goods['details_pic'] ?? '';
                 $item['banner'] = $goods['banner'] ?? '';
@@ -79,6 +81,9 @@ class V2Client extends V2BaseClient
                 $item['g_desc'] = $goods['desc'] ?? '';
                 $item['gc_id'] = $goods['gc_id'] ?? "";
                 $item['gc_name'] = $goods['gc_name'] ?? "";
+                $item['length'] = $goods['length'] ?? "";
+                $item['width'] = $goods['width'] ?? "";
+                $item['height'] = $goods['height'] ?? "";
                 return $item;
             });
             if ($data) {
@@ -127,8 +132,7 @@ class V2Client extends V2BaseClient
                     'machine_id');
                 $machine['sale_income'] = ($sdc['totalPrice'] ?? 0) - ($sdc['totalRefundMoney'] ?? 0) - ($sdc['totalDiscountPrice'] ?? 0);
                 $machine['sale_count'] = ($sdc['totalQuantity'] ?? 0) - ($sdc['totalRefundQuantity'] ?? 0);
-                unset($machine['country_id'], $machine['state_id
-                '], $machine['city_id'], $machine['regions_id'], $machine['scene_id']);
+                unset($machine['country_id'], $machine['state_id'], $machine['city_id'], $machine['regions_id'], $machine['scene_id']);
 
                 if ($machine['device_type'] == 2) $machine['oo_status'] = "online";
                 return $machine;
