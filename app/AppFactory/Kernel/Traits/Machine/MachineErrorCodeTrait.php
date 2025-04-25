d7376a24f90ba21aa93ddd8e1f20e72341ccd2c1
Ted <272897521@qq.com>
2025-04-25 14:44:02 +0800

    调整故障码上报分类

--- app/AppFactory/Kernel/Traits/Machine/MachineErrorCodeTrait.php
+++ app/AppFactory/Kernel/Traits/Machine/MachineErrorCodeTrait.php
@@ -38,12 +38,20 @@ trait MachineErrorCodeTrait
     {
         actionLog($this->message,'故障码上报');
         $this->getMachineAddress();
+        $position = 1;
+        if (isset($this->message['error_position']) && $this->message['error_position']) {
+            $position = $this->message['error_position'];
+        }  else {
+            $len = strlen($this->message['errorCode']);
+            if ($len == 9) $position = 2;
+            if ($len == 7) $position = 3;
+        }
         $insert = [
             "m_id" => $this->machine['m_id'],
             "machine_id" => $this->machine['machine_id'],
             "machine_name" => $this->machine['machine_name'],
             "address" => $this->machine['address'] ?? "",
-            "error_position" => $this->message['error_position'] ?? 1,
+            "error_position" => $position,
             "errorCode" => $this->message['errorCode'],
             "remark" => $this->lang("deviceErrorCode." . $this->message['errorCode']),
             "msg" => $this->message['msg'] ?? "",
