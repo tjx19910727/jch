3ece0057f79c645e5786ecdccd9dada5a7c9aaa0
Ted <272897521@qq.com>
2025-06-10 18:36:07 +0800

    调整POS机支付回调处理支付失败BUG

--- app/AppFactory/Pay/Notify/PosClient.php
+++ app/AppFactory/Pay/Notify/PosClient.php
@@ -19,15 +19,16 @@ class PosClient extends PayBaseClient
 {
     use AfterOrderPaymentTrait;
     use SaleOrdersRevenueTrait;
+
     public function __construct(ServiceContainer $app)
     {
         parent::__construct($app);
         $this->data = $app->getConfig();
         if (isset($this->data['machine_id']) && $this->data['machine_id'])
             $this->machine = $this->getMachineFind(['machine_id' => $this->data['machine_id']]);
-        $check = SignUtil::checkSign($this->data,$this->machine['signKey']);
+        $check = SignUtil::checkSign($this->data, $this->machine['signKey']);
         if ($check !== true) {
-            $this->r(300,$this->lang("VPos.check_sign_fail"))->send();
+            $this->r(300, $this->lang("VPos.check_sign_fail"))->send();
             die();
         }
     }
@@ -39,35 +40,31 @@ class PosClient extends PayBaseClient
     public function handlePos()
     {
         $message = $this->data;
+        $tradeNo = $message['trade_no'];
+        $this->order = $this->getSaleOrdersFind(['trade_no' => $tradeNo]);
+        actionLog($this->getLS(), '【SQL】查询订单');
+        actionLog($this->order, '订单数据');
+        if (!$this->order) return $this->rFail($this->lang("VPos.order_no_data"));
         // pay_status，支付状态，3：已支付，5：取消支付
         if ($message['payment_status'] === 'TRADE_SUCCESS') {
-            $tradeNo = $message['trade_no'];
-            $this->order = $this->getSaleOrdersFind(['trade_no' => $tradeNo]);
-            actionLog($this->getLS(),'【SQL】查询订单');
-            actionLog($this->order,'订单数据');
-            if ($this->order) {
-                $this->order = $this->order->toArray();
-                if ($this->order['pay_status'] != 3) {
-                    $this->order['mch_no'] = $message['mch_no'];
-                    $this->startTrans();
-                    // 结算分润收益
-                    try {
-                        $flag[] = $this->settlementRevenue();
-                        $flag[] = $this->paymentSuccessful();
-                        actionLog($flag,'flag');
-                        $result = flag_check($flag);
-                        $result = $this->checkTrans($result);
-                        actionLog($result, '支付成功处理事务结果');
-                        return $result;
-                    } catch (\Exception $e) {
-                        $this->rollbackTrans();
-                        actionException($e, 1);
-                        return $this->rTryCatch($e->getMessage());
-                    }
+            $this->order = $this->order->toArray();
+            if ($this->order['pay_status'] != 3) {
+                $this->order['mch_no'] = $message['mch_no'];
+                $this->startTrans();
+                // 结算分润收益
+                try {
+                    $flag[] = $this->settlementRevenue();
+                    $flag[] = $this->paymentSuccessful();
+                    actionLog($flag, 'flag');
+                    $result = flag_check($flag);
+                    $result = $this->checkTrans($result);
+                    actionLog($result, '支付成功处理事务结果');
+                    return $result;
+                } catch (\Exception $e) {
+                    $this->rollbackTrans();
+                    actionException($e, 1);
+                    return $this->rTryCatch($e->getMessage());
                 }
-            } else {
-                actionLog($this->getLS(), "没有查到订单");
-                return $this->r(300,$this->lang("VCoGoLink.order_no_data"));
             }
         } else {
             $result = $this->paymentFailed();
