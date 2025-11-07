82507a6d62dd87a70cf340b879fa6b3c4453c9bb
18160689 <chnie@karrie.com>
2025-11-07 17:52:16 +0800

    提交库存报表生成接口微调

--- app/AppFactory/Kernel/Traits/Machine/MachineChannelStockReportTrait.php
+++ app/AppFactory/Kernel/Traits/Machine/MachineChannelStockReportTrait.php
@@ -17,4 +17,9 @@ trait MachineChannelStockReportTrait
     {
         return MachineChannelStockReportView::getList($where,$pageNum,$field,$order,'',$group);
     }
+
+    public function getMachineChannelStockReportJoinMchList($where,$pageNum = 0,$field = "*", $order = "", $group = "")
+    {
+        return MachineChannelStockReportView::joinMachineList($where,$pageNum,$field,$order,'',$group);
+    }
 }
