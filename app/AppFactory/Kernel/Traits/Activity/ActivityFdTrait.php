abbd4f4377fede304b66e262b9777b5364beaeff
jixiang <1013598556@qq.com>
2025-11-19 16:07:56 +0800

    update

--- app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
+++ app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
@@ -66,7 +66,7 @@ trait ActivityFdTrait
         $goodsField = "mg.g_id as g_id";
         $machineGoods = MachineGoodsModel::getMGoodsListJoinGoods($where2, 0, $goodsField)->toArray();
         $machineGoodsIds = array_column($machineGoods,'g_id');
-
+        actionLog($machineGoodsIds,'在售商品id');
 
         $where = 'am.m_id = ' . $this->machine['m_id'] . " AND status < 3 AND start_date < " . time();
         $fdList = $this->getActivityFdListByMachine($where,'fd_id,fd_name,start_date,end_date,fd_type,condition_type,desc,status');
