ad291b25cc0ef99d972aa866af544c860cda653e
jixiang <1013598556@qq.com>
2025-11-19 09:08:44 +0800

    Model类引入

--- app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
+++ app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
@@ -8,7 +8,7 @@
 
 namespace app\AppFactory\Kernel\Traits\Activity;
 use app\AppFactory\Kernel\Model\Machine\MachineGoodsModel;
-
+use app\AppFactory\Kernel\Model\Activity\Fd\ActivityFdContentModel;
 
 use app\AppFactory\Kernel\Model\Activity\Fd\ActivityFdModel;
 
@@ -81,7 +81,8 @@ trait ActivityFdTrait
                     $fieldOrder = "fdc_sort DESC, condition_value1 asc, fdc_id asc";
                     $field = "fdc_id,CAST(condition_value AS UNSIGNED) condition_value1, condition_value,g_id,g_name,pic,sku,gc_id,gc_name,active_value,fdc_sort";
                 }
-                $fdl['content'] = $this->getActivityFdContentList(['fd_id' => $fdl['fd_id'], ['g_id', 'in', $machineGoodsIds]],0,$field,$fieldOrder);
+                // $fdl['content'] = $this->getActivityFdContentList(['fd_id' => $fdl['fd_id']],0,$field,$fieldOrder);
+                $fdl['content'] = ActivityFdContentModel::getList(['fd_id' => $fdl['fd_id'], ['g_id', 'in', $machineGoodsIds]],0,$field,$fieldOrder);
                 if ($fdl['status'] == 1) $update['status'] = 2;
                 if ($fdl['end_date'] > 0 && $fdl['end_date'] < strtotime(date("Y-m-d")) && $fdl['status'] != 3) {
                     $update['status'] = 3;
