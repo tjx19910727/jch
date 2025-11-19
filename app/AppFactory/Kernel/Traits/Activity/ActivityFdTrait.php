29fbc0af2a11f3159a33c894b65283c524b7a3d5
jixiang <1013598556@qq.com>
2025-11-19 10:17:25 +0800

    判断变量

--- app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
+++ app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
@@ -62,7 +62,7 @@ trait ActivityFdTrait
     public function getActivityFdByMachine()
     {
         //todo  匹配售卖机当前商品，仅展示匹配成功的活动机及商品
-        // $this->machine['m_id'] = '127';
+        $this->machine['m_id'] = '127';
         $where2['mg.m_id'] = $this->machine['m_id'];
         $goodsField = "mg.g_id as g_id";
         $machineGoods = MachineGoodsModel::getMGoodsListJoinGoods($where2, 0, $goodsField)->toArray();
