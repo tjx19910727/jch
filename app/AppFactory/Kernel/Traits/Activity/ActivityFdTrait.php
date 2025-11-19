3201a586e3c51b3c765ed3dab5537ddf8c0fa06e
jixiang <1013598556@qq.com>
2025-11-19 12:01:13 +0800

    跳过sql查询调试

--- app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
+++ app/AppFactory/Kernel/Traits/Activity/ActivityFdTrait.php
@@ -63,11 +63,11 @@ trait ActivityFdTrait
     {
         //todo  匹配售卖机当前商品，仅展示匹配成功的活动机及商品
         // $this->machine['m_id'] = '127';
-        $where2['mg.m_id'] = $this->machine['m_id'];
-        $goodsField = "mg.g_id as g_id";
-        $machineGoods = MachineGoodsModel::getMGoodsListJoinGoods($where2, 0, $goodsField)->toArray();
-        $machineGoodsIds = array_column($machineGoods,'g_id');
-
+        // $where2['mg.m_id'] = $this->machine['m_id'];
+        // $goodsField = "mg.g_id as g_id";
+        // $machineGoods = MachineGoodsModel::getMGoodsListJoinGoods($where2, 0, $goodsField)->toArray();
+        // $machineGoodsIds = array_column($machineGoods,'g_id');
+        $machineGoodsIds = ['883','119','114','789','1234'];
         $where = 'am.m_id = ' . $this->machine['m_id'] . " AND status < 3 AND start_date < " . time();
         $fdList = $this->getActivityFdListByMachine($where,'fd_id,fd_name,start_date,end_date,fd_type,condition_type,desc,status');
         if ($fdList) {
