24e4f305abbbd50630d31f43a44eef24267480c9
jixiang <1013598556@qq.com>
2025-12-02 15:55:09 +0800

    数据库文件更新

---
+++ app/AppFactory/Kernel/Model/Wx/WechatMenuModel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by VSCODE.
+ * User: Alex
+ * Date: 2025/12/01
+ * Time: 11:56
+ */
+
+namespace app\AppFactory\Kernel\Model\Wx;
+
+
+use app\AppFactory\Kernel\Model\BaseModel;
+
+class WechatMenuModel extends BaseModel
+{
+    protected $pk = "id";
+    protected $name = "wechat_menu";
+
+}
