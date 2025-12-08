1efbfaf7b5c7ab7aff380b864d377291ba5660ac
jixiang <1013598556@qq.com>
2025-12-08 10:51:30 +0800

    小车订单退款后，后台无法查询订单问题处理

--- app/AppFactory/Management/Sale/SaleOrdersClient.php
+++ app/AppFactory/Management/Sale/SaleOrdersClient.php
@@ -85,7 +85,15 @@ class SaleOrdersClient extends ManagementClient
                     if ($mIds) $where[] = ['m_id', 'in', $mIds];
                 }
             }
-            $data = $this->getSaleOrdersList($where, $pageNum, $field, $order, $supplier);
+            $data = $this->getSaleOrdersList($where, $pageNum, $field, $order, $supplier)->toArray();
+            //因为小车退款订单无法查询，这里对查询结果处理一下
+            $new_data = [];
+            foreach($data['data'] as $v){
+                if($v['pay_status'] == 3 || $v['pay_type'] == 7){
+                    $new_data[] = $v;
+                }
+            }
+            $data['data'] = $new_data;
             return $this->r(200, $this->lang("query_success"), $data);
         } catch (\Exception $e) {
             actionException($e, 1);
@@ -303,7 +311,7 @@ class SaleOrdersClient extends ManagementClient
             if ($mIds) $where[] = ['m_id', 'in', $mIds];
         }
         $list = $this->getSaleOrdersList($where, 0,
-            'order_id,machine_id,machine_name,trade_no,mch_no,total_quantity,total_price,discount_price,retail_price,factory,inventory_location,
+            'order_id,machine_id,machine_name,pay_status,trade_no,mch_no,total_quantity,total_price,discount_price,retail_price,factory,inventory_location,
                 (CASE order_type 
                     WHEN 1 THEN "普通订单"
                     WHEN 2 THEN "优惠券订单"
@@ -403,6 +411,12 @@ class SaleOrdersClient extends ManagementClient
                 WHEN 0 THEN "免支付" END) pay_type,
                 FROM_UNIXTIME(sor.update_time,"%Y-%m-%d %H:%i:%s") pay_time,("-") out_time', 'sor.update_time asc');
             if ($refund) $list = array_merge($list, $refund->toArray());
+            $new_list = [];
+            foreach($list as $v) {
+                if($v['pay_status'] == 3 || $v['pay_type'] == 7){
+                    $new_list[] = $v;
+                }
+            }
             $title = [
                 "order_id" => "订单ID",
                 "machine_id" => "设备编号",
@@ -422,7 +436,7 @@ class SaleOrdersClient extends ManagementClient
                 "out_time" => "出货时间",
             ];
             $filename = "订单交易-" . date("Ymd");
-            return $this->sendToExport("订单管理-销售订单", $filename, $title, $list);
+            return $this->sendToExport("订单管理-销售订单", $filename, $title, $new_list);
         }
         return $this->rFail($this->lang("action_fail"));
     }
