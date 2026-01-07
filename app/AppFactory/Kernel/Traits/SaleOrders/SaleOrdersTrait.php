bbcc86155b09eef059bcabb9d369f9a520d4a8a0
jixiang <1013598556@qq.com>
2026-01-07 15:50:37 +0800

    update

--- app/AppFactory/Kernel/Traits/SaleOrders/SaleOrdersTrait.php
+++ app/AppFactory/Kernel/Traits/SaleOrders/SaleOrdersTrait.php
@@ -436,7 +436,9 @@ trait SaleOrdersTrait
     {
         if (strstr($this->message['trade_no'], "remote_out_goods")) {
             actionLog($this->message, "远程出货视频保存地址记录执行");
-            return SaleOrdersDetailsModel::update(['reote_out_goods_video' => $this->message['transaction_video']], ['trade_no' => $this->message['trade_no']]);
+            $sod_id = str_replace("remote_out_goods_", "", $this->message['trade_no']);
+            $sod_id = intval($sod_id);
+            return SaleOrdersDetailsModel::update(['reote_out_goods_video' => $this->message['transaction_video']], ['sod_id' => $sod_id]);
         }
         if (strstr($this->message['trade_no'], "door_open")) {
             actionLog($this->message, "开门视频保存地址记录执行");
