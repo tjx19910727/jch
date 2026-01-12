74bafb291ed56377797d6decd5febdfa2fb5fd2a
jixiang <1013598556@qq.com>
2026-01-12 12:29:24 +0800

    统一入口

--- app/AppFactory/Kernel/Providers/Management/WeiChengProvider.php
+++ app/AppFactory/Kernel/Providers/Management/WeiChengProvider.php
@@ -11,23 +11,15 @@ namespace app\AppFactory\Kernel\Providers\Management;
 
 use app\AppFactory\Kernel\Container;
 use app\AppFactory\Kernel\ServiceProviderInterface;
-use app\AppFactory\Management\WeiCheng\WcGoodsClient;
-use app\AppFactory\Management\WeiCheng\WcGoodsTypesClient;
-use app\AppFactory\Management\WeiCheng\WcRequestLogsClient;
+use app\AppFactory\Management\WeiCheng\WeiChengClient;
 
 class WeiChengProvider implements ServiceProviderInterface
 {
     public function register(Container $app)
     {
         // TODO: Implement register() method.
-        $app['WcGoods'] = function ($app) {
-            return new WcGoodsClient($app);
-        };
-        $app['WcGoodsTypes'] = function ($app) {
-            return new WcGoodsTypesClient($app);
-        };
-        $app['WcRequestLogs '] = function ($app) {
-            return new WcRequestLogsClient($app);
+        $app['weicheng'] = function ($app) {
+            return new WeiChengClient($app);
         };
     }
                                                                                                                                                                                                                                                                                         }
