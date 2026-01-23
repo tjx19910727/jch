3fb8131899d83b3092513e66c1d1bcc3bcaf0b01
jixiang <1013598556@qq.com>
2026-01-23 19:25:00 +0800

    退款问题处理

--- app/AppFactory/Kernel/Traits/Payment/AfterOrderRefundTrait.php
+++ app/AppFactory/Kernel/Traits/Payment/AfterOrderRefundTrait.php
@@ -119,8 +119,10 @@ trait AfterOrderRefundTrait
     {
         if($this->order['pay_type'] == 9){
             $refund_points = bcmul($this->order['intergral_rate'], $this->refund['refund_amount']);
-        // }elseif($this->sod['total_sod_points']){
-        //     $refund_points = bcmul(bcdiv($this->refund['refund_quantity'], $this->sod['quantity'], 0),$this->sod['total_sod_points'],2);
+        }elseif($this->order['total_points']){
+            $saleOrdersDetails = $this->getSaleOrdersDetailsFind(['sod_id' => $this->refund['sod_id']]);
+            if(!$saleOrdersDetails) $refund_points = 0;
+            $refund_points = bcmul(bcdiv($this->refund['refund_quantity'], $this->saleOrdersDetails['quantity'], 0),$this->saleOrdersDetails['total_sod_points'],2);
         }
         $flag[] = $this->incSaleOrdersDetails(['sod_id' => $this->refund['sod_id']], 'refund_points', $refund_points);
         actionLog($this->getLS(),'修改订单副表退款积分SQL');
