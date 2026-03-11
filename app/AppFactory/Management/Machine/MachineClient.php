c4b190f9663a8506ccd6757516952488effcfd52
18163326 <1924278398@qq.com>
2026-03-11 15:36:15 +0800

    修复设备详情接口

--- app/AppFactory/Management/Machine/MachineClient.php
+++ app/AppFactory/Management/Machine/MachineClient.php
@@ -261,6 +261,7 @@ class MachineClient extends ManagementClient
             if (isset($item['state_id']) && $item['state_id']) $item['state'] = $this->getEarthStatesFind(['id' => $item['state_id']],'code,name,cname');
             if (isset($item['city_id']) && $item['city_id']) $item['city'] = $this->getEarthCitiesFind(['id' => $item['city_id']],'code,name,cname');
             if (isset($item['regions_id']) && $item['regions_id']) $item['regions'] = $this->getEarthRegionsFind(['id' => $item['regions_id']],'code,name,cname');
+            if (isset($item['machine_level']) && $item['machine_level']) $item['machine_level_info'] = $this->getMachineLevelFind(['machine_level' => $item['machine_level']],'name,pic');
         }
         return $this->rQ($item);
     }
