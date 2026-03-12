e3649d333cb779dabc365209acbe4027c3b60cd8
jixiang <1013598556@qq.com>
2026-03-12 08:31:30 +0800

    货道租赁model文件提交

---
+++ app/AppFactory/Kernel/Traits/Auth/AuthOrgMachineChannelTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 2024/3/9
+ * Time: 14:45
+ */
+
+namespace app\AppFactory\Kernel\Traits\Auth;
+
+
+use app\AppFactory\Kernel\Model\Auth\AuthOrgMachineChannelModel;
+
+trait AuthOrgMachineChannelTrait
+{
+
+    public function getAuthOrgMCList($where, $column)
+    {
+        return AuthOrgMachineChannelModel::getList($where, $column);
+    }
+
+    public function getAuthOrgMCColumn($where, $column)
+    {
+        return AuthOrgMachineChannelModel::getColumn($where, $column);
+    }
+
+
+    public function addAuthOrgMC($insert)
+    {
+        $data = AuthOrgMachineChannelModel::create($insert);
+        return $data->id;
+    }
+
+    public function updateAuthOrgMC($update, $where = [], $field = [])
+    {
+        return AuthOrgMachineChannelModel::update($update, $where, $field);
+    }
+
+    public function delAuthOrgMC($where)
+    {
+        return AuthOrgMachineChannelModel::whereDel($where);
+    }
+}
