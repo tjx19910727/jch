68ff555f2a8a4c6e9223bf9958aea52bad2ce8bb
18163326 <1924278398@qq.com>
2026-04-01 19:27:19 +0800

    余额支付修复

--- app/AppFactory/Kernel/Traits/Card/CardBalanceBucketsTrait.php
+++ app/AppFactory/Kernel/Traits/Card/CardBalanceBucketsTrait.php
@@ -23,13 +23,9 @@ trait CardBalanceBucketsTrait
      */
     protected function getCardBalanceBucketSummaryRow($card_no, $now)
     {
+        $now = intval($now);
         return CardBalanceBucketsModel::where('card_no', $card_no)
-            ->fieldRaw("\
-                IFNULL(SUM(CASE WHEN (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS available_balance,\
-                IFNULL(SUM(CASE WHEN amount_type = 1 AND (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS principal_balance,\
-                IFNULL(SUM(CASE WHEN amount_type = 2 AND (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS gift_balance,\
-                IFNULL(SUM(CASE WHEN refund_eligible = 1 AND (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS refundable_balance\
-            ")
+            ->fieldRaw("IFNULL(SUM(CASE WHEN (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS available_balance, IFNULL(SUM(CASE WHEN amount_type = 1 AND (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS principal_balance, IFNULL(SUM(CASE WHEN amount_type = 2 AND (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS gift_balance, IFNULL(SUM(CASE WHEN refund_eligible = 1 AND (expire_at = 0 OR expire_at >= {$now}) THEN remain_amount ELSE 0 END),0) AS refundable_balance")
             ->find();
     }
 
