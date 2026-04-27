f8990b71c2e492d853f80a1f2f94a863c3a3c098
18163326 <1924278398@qq.com>
2026-04-27 15:52:59 +0800

    榫卯相关流程调整

--- app/management/controller/machine/SimCardInfo.php
+++ app/management/controller/machine/SimCardInfo.php
@@ -9,6 +9,7 @@
 namespace app\management\controller\machine;
 
 
+use app\AppFactory\Kernel\Support\SimiotService\Simiot;
 use app\management\controller\Common;
 
 class SimCardInfo extends Common
@@ -35,5 +36,45 @@ class SimCardInfo extends Common
         $postData = input();
         $where = $this->getWhere($postData, false, []);
         return $this->app->simCardInfo->getFindData($where, $this->field);
+    }
+
+    /**
+     * 获取 sim_card_machine 列表
+     * @return array|\think\response\Json
+     */
+    public function getMachineList()
+    {
+        $postData = input();
+        $pageNum = $postData['pageNum'] ?? 0;
+        $where = $this->getWhere($postData, false, [
+            'machine_id' => 'like',
+            'iccid' => 'like',
+            'remark' => 'like',
+        ]);
+        return $this->app->simCardInfo->getMachineListData($where, $pageNum, $this->field, 'id desc');
+    }
+
+    /**
+     * 获取 sim_card_machine 单条数据
+     * @return array|\think\response\Json
+     */
+    public function getMachineFind()
+    {
+        $postData = input();
+        $where = $this->getWhere($postData, false, []);
+        return $this->app->simCardInfo->getMachineFindData($where, $this->field);
+    }
+
+    /**
+     * 获取流量池信息
+     * @return array|\think\response\Json
+     */
+    public function queryPool()
+    {
+        $result = Simiot::queryPool();
+        if (is_array($result) && isset($result['code']) && intval($result['code']) === 0) {
+            return returnState(200, lang('query_success'), $result['result'][0] ?? []);
+        }
+        return returnState(100, $result['message'] ?? lang('query_fail'), $result);
     }
 }
