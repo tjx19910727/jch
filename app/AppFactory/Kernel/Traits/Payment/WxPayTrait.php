889532e673453f52d6fdbb160a21d8885dacad22
jixiang <1013598556@qq.com>
2026-05-19 17:16:52 +0800

    update

--- app/AppFactory/Kernel/Traits/Payment/WxPayTrait.php
+++ app/AppFactory/Kernel/Traits/Payment/WxPayTrait.php
@@ -180,7 +180,6 @@ trait WxPayTrait
             if (isset($result['code_url'])) {
                 $this->returnData['order'] = $this->order;
                 $this->returnData['paymentUrlLink'] = $result['code_url'];
-                $this->returnData['qrCodeLink'] = $result['code_url'];
                 $this->returnData['result'] = $result;
                 return $this->r(200, 'SUCCESS', $this->returnData);
             }
