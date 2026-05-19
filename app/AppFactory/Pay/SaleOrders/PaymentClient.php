34bfccbb496d1254477b4e7635755d2d8bd7fc4e
jixiang <1013598556@qq.com>
2026-05-19 16:39:07 +0800

    新增日志

--- app/AppFactory/Pay/SaleOrders/PaymentClient.php
+++ app/AppFactory/Pay/SaleOrders/PaymentClient.php
@@ -203,11 +203,21 @@ class PaymentClient extends PayBaseClient
                 'dispatch_method' => $func_name,
             ], '支付方法路由分派');
             $result = $this->$func_name();
+            $resultCode = null;
+            $resultMsg = null;
+            if (is_array($result)) {
+                $resultCode = $result['code'] ?? null;
+                $resultMsg = $result['msg'] ?? null;
+            } elseif (is_object($result)) {
+                $resultCode = $result->code ?? null;
+                $resultMsg = $result->msg ?? null;
+            }
             actionLog([
                 'order_id' => $this->order['order_id'] ?? null,
                 'dispatch_method' => $func_name,
-                'result_code' => $result['code'] ?? null,
-                'result_msg' => $result['msg'] ?? null,
+                'result_type' => is_object($result) ? get_class($result) : gettype($result),
+                'result_code' => $resultCode,
+                'result_msg' => $resultMsg,
             ], '支付方法路由返回');
             return $result;
         }
