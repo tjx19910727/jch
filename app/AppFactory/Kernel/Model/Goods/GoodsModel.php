800975c58c2042786606e1e4be28951b2910f351
18163326 <1924278398@qq.com>
2026-06-05 16:47:51 +0800

    商品库修复

--- app/AppFactory/Kernel/Model/Goods/GoodsModel.php
+++ app/AppFactory/Kernel/Model/Goods/GoodsModel.php
@@ -21,17 +21,6 @@ class GoodsModel extends BaseModel
 {
     protected $name = "goods";
     protected $pk = "g_id";
-    protected static $disableQueuePushOnce = false;
-
-    public static function disableQueuePushOnce()
-    {
-        self::$disableQueuePushOnce = true;
-    }
-
-    public static function clearDisableQueuePushFlag()
-    {
-        self::$disableQueuePushOnce = false;
-    }
 
 //    protected $schema = [
 //        "g_id" => "int",
@@ -136,23 +125,19 @@ class GoodsModel extends BaseModel
      * 修改后自动将商品ID放入队列，由守护进程去后台更新设备货道与设备商品库，两个位置在修改后会自动触发终端更新
      * @param Model $model
      */
-    protected static function onAfterUpdate(Model $model)
-    {
-        if (self::$disableQueuePushOnce) {
-            self::$disableQueuePushOnce = false;
-            return;
-        }
-        $redis = new \Redis();
-        $config = config("redis");
-        $redis->connect($config['host'], $config['port'],$config['timeout'],$config['reserved'],$config['retry_interval']);
-        if (isset($config['password']) && $config['password']) $redis->auth($config['password']);
-        $redis->lPush("updateGoods",$model['g_id']);
-        $redis->expire("updateGoods",300);
-        $redisData = $redis->lRange("updateGoods", 0, -1);
-        actionLog($model['g_id'],'修改的商品ID');
-        actionLog($redisData,'放入Redis数据');
-        $redis->close();
-    }
+//    protected static function onAfterUpdate(Model $model)
+//    {
+//        $redis = new \Redis();
+//        $config = config("redis");
+//        $redis->connect($config['host'], $config['port'],$config['timeout'],$config['reserved'],$config['retry_interval']);
+//        if (isset($config['password']) && $config['password']) $redis->auth($config['password']);
+//        $redis->lPush("updateGoods",$model['g_id']);
+//        $redis->expire("updateGoods",300);
+//        $redisData = $redis->lRange("updateGoods", 0, -1);
+//        actionLog($model['g_id'],'修改的商品ID');
+//        actionLog($redisData,'放入Redis数据');
+//        $redis->close();
+//    }
 
     /**
      * 添加后自动将商品ID放入队列，由守护进程触发通知终端更新商品
