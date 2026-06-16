3cab69c4edf4919d25e2391fbf6a389cecbbabd1
18163326 <1924278398@qq.com>
2026-06-16 09:58:27 +0800

    商品保质期

--- app/AppFactory/Management/Machine/MachineChannelClient.php
+++ app/AppFactory/Management/Machine/MachineChannelClient.php
@@ -493,6 +493,19 @@ class MachineChannelClient extends ManagementClient
                 $this->addGoodsChange($insertGc);
             }
         }
+        if (!empty($postData['manufacture_time'])) {
+            $postData['manufacture_time'] = strtotime(date('Y-m-d 23:59:59', strtotime($postData['manufacture_time'])));
+        }
+        //如果有传入生产日期，expire_time根据生产日期和商品表的保质期自动计算得出
+        if (isset($postData['manufacture_time']) && $postData['manufacture_time'] > 0 && isset($postData['g_id']) && $postData['g_id'] > 0) {
+            $shelfLife = $this->getGoodsValue(['g_id' => $postData['g_id']], 'sell_by_date');
+            if ($shelfLife) {
+                $postData['expire_time'] = strtotime(date('Y-m-d 23:59:59', $shelfLife * 24 * 3600 + $postData['manufacture_time']));
+            }else{
+                $postData['expire_time'] = 0;
+            }
+        }
+
         $result = $this->updateMachineChannel($postData);
         if ($result) {
             // 发送触发货道更新数据,如果是边柜货道不发送
@@ -692,8 +705,8 @@ class MachineChannelClient extends ManagementClient
         if(isset($postData['stock_warning'])){
             $updateData['stock_warning'] = $postData['stock_warning'] < 0 ? 0 : $postData['stock_warning'];
         }
-        if(isset($postData['expire_time'])){
-            $updateData['expire_time'] = $postData['expire_time'];
+        if(!empty($postData['expire_time'])){
+            $updateData['expire_time'] = date('Y-m-d 23:59:59', strtotime($postData['expire_time']));
         }
         if (!$updateData) return $this->r(100, $this->lang("action_fail"));
 
