a572169315599f0b177016cea802235529153920
18163326 <1924278398@qq.com>
2026-06-16 15:12:27 +0800

    打印小票问题修复

--- config/receipt.php
+++ config/receipt.php
@@ -12,7 +12,5 @@ return [
         "content" => [
             "" => "",
         ],
-    ],
-    "receipt_code1" => "/uploads/adv/20250618/41da4aa9c2e34fb84b123c8d39eba214.png",
-    "receipt_code2" => "/uploads/adv/20251022/0e543e3d6e1861bd59fed97194fd3f3f.jpg",
+    ]
 ];
