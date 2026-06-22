99d54d98835d405b457cc58038c9b644d9b18df0
18163326 <1924278398@qq.com>
2026-06-22 17:40:40 +0800

    语法错误不生效

--- app/AppFactory/Management/Machine/MachineClient.php
+++ app/AppFactory/Management/Machine/MachineClient.php
@@ -589,6 +589,7 @@ class MachineClient extends ManagementClient
             $item['machine_on_off'] = $machineOnOff;
             $item['sleep_status'] = 1;
             if(!empty($item['current_status']) && $item['current_status'] == 'sleep'){
+                //状态设置为休眠
                 $item['sleep_status'] = 2;
             }
             if (isset($item['country_id']) && $item['country_id']) $item['country'] = $this->getEarthCountriesFind(['id' => $item['country_id']],'code,name,cname');
