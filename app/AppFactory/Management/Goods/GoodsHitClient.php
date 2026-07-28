0bb0f844089a19e1443a299b4b525537bbbb29c1
18163326 <1924278398@qq.com>
2026-07-28 14:34:22 +0800

    测试修复

--- app/AppFactory/Management/Goods/GoodsHitClient.php
+++ app/AppFactory/Management/Goods/GoodsHitClient.php
@@ -168,7 +168,7 @@ class GoodsHitClient extends ManagementClient
     protected function getGoodsBehaviorTrackingAggregateFields()
     {
         return "MAX(IF(gbt.is_online = 1, CONVERT(wg.g_name USING utf8mb4) COLLATE utf8mb4_unicode_ci, CONVERT(g.g_name USING utf8mb4) COLLATE utf8mb4_unicode_ci)) g_name,"
-            . "MAX(IF(gbt.is_online = 1, CONVERT(wg.sku USING utf8mb4) COLLATE utf8mb4_unicode_ci, CONVERT(g.sku USING utf8mb4) COLLATE utf8mb4_unicode_ci)) sku,"
+            . "MAX(IF(gbt.is_online = 1, CONVERT(wg.out_no USING utf8mb4) COLLATE utf8mb4_unicode_ci, CONVERT(g.sku USING utf8mb4) COLLATE utf8mb4_unicode_ci)) sku,"
             . "MAX(IF(gbt.is_online = 1, CONVERT(wg.gc_name USING utf8mb4) COLLATE utf8mb4_unicode_ci, CONVERT(g.gc_name USING utf8mb4) COLLATE utf8mb4_unicode_ci)) gc_name,"
             . "MAX(IF(gbt.is_online = 1, CONVERT(wg.out_no USING utf8mb4) COLLATE utf8mb4_unicode_ci, NULL)) goods_out_no";
     }
@@ -337,7 +337,7 @@ class GoodsHitClient extends ManagementClient
             if ($field == 'g_id') $value[0] = 'gbt.goods_id';
             if ($field == 'm_id') $value[0] = 'gbt.m_id';
             if ($field == 'machine_id') $value[0] = 'gbt.machine_id';
-            if ($field == 'sku') $value[0] = Db::raw('IF(gbt.is_online = 1, wg.sku, g.sku)');
+            if ($field == 'sku') $value[0] = Db::raw('IF(gbt.is_online = 1, wg.out_no, g.sku)');
             if ($field == 'create_time') {
                 $value[0] = 'gbt.device_created_at';
             }
