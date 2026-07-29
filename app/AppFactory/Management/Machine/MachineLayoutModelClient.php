46c96f39039705df27433b1651c901de7319e688
涂吉祥 <1013598556@qq.com>
2026-07-29 18:01:37 +0800

    接口添加字段

--- app/AppFactory/Management/Machine/MachineLayoutModelClient.php
+++ app/AppFactory/Management/Machine/MachineLayoutModelClient.php
@@ -30,7 +30,7 @@ class MachineLayoutModelClient extends ManagementClient
     public function getList($where = [], $pageNum = 0, $field = "*", $order = "mlm_id desc", $rQ = 1)
     {
         if ($field == "*") {
-            $field = "mlm_id,model_name,machine_level,IFNULL((SELECT name FROM machine_level_desc WHERE machine_level = machine_layout_model.machine_level LIMIT 1),'') machine_level_desc,inner_width,inner_height,inner_depth,shelf_thickness,divider_thickness,left_indent,right_indent,channel_width,custom_channel_widths,channel_height,channel_depth,total_rows,total_cols,actual_channel_width,status,create_time,update_time";
+            $field = "mlm_id,model_name,machine_level,IFNULL((SELECT name FROM machine_level_desc WHERE machine_level = a.machine_level LIMIT 1),'') machine_level_desc,inner_width,inner_height,inner_depth,shelf_thickness,divider_thickness,left_indent,right_indent,channel_width,custom_channel_widths,channel_height,channel_depth,total_rows,total_cols,actual_channel_width,status,create_time,update_time";
         }
         return $this->rQ($this->getMachineLayoutModelList($where, $pageNum, $field, $order));
     }
