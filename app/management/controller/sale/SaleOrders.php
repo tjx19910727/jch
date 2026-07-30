60bd650b7075e03ab08eb4ff08d8a4a1709ed400
涂吉祥 <1013598556@qq.com>
2026-07-30 12:38:22 +0800

    移除订单pay_channel旧字段依赖

--- app/management/controller/sale/SaleOrders.php
+++ app/management/controller/sale/SaleOrders.php
@@ -40,7 +40,7 @@ class SaleOrders extends Common
             $postData['pay_time'] = $postData['create_date'];
         }
         unset($postData['create_date']);
-        $where = $this->getWhere($postData,false,['trade_no' => "like","order_type" => "in","mch_no" => "like","machine_name" => "like","machine_id" => "like","pay_type" => "in","pay_channel" => "in",'factory'=>'in','inventory_location'=>'in','out_status'=>'in','run_mode'=>'in']);
+        $where = $this->getWhere($postData,false,['trade_no' => "like","order_type" => "in","mch_no" => "like","machine_name" => "like","machine_id" => "like","pay_type" => "in",'factory'=>'in','inventory_location'=>'in','out_status'=>'in','run_mode'=>'in']);
         $where['raw'] = "pay_status in ('3', '7')";
         $authMch = $this->authMchCannel();
         if($authMch['status'] != 0){
@@ -60,7 +60,7 @@ class SaleOrders extends Common
         $hasCostPriceAuth = $this->hasCostPriceAuth();
 
         $costPriceField = $hasCostPriceAuth ? "cost_price" : "0 cost_price";
-        $field = "order_id,trade_no,mch_no,total_quantity,total_price,total_points,discount_price,retail_price,out_status,http_out_status,order_type,pay_type,pay_method,pay_channel,pay_channel_name,user_id,out_trade_no,pay_status,pay_time,out_time,machine_name,a.m_id,a.machine_id,a.machine_level,a.run_mode,(CASE a.run_mode WHEN 2 THEN '测试模式' ELSE '生产模式' END) run_mode_desc,
+        $field = "order_id,trade_no,mch_no,total_quantity,total_price,total_points,discount_price,retail_price,out_status,http_out_status,order_type,pay_type,pay_method,user_id,out_trade_no,pay_status,pay_time,out_time,machine_name,a.m_id,a.machine_id,a.machine_level,a.run_mode,(CASE a.run_mode WHEN 2 THEN '测试模式' ELSE '生产模式' END) run_mode_desc,
         factory,inventory_location,has_hotel,refund_status,(total_price - refund_amount) total_price, (total_cost_points - refund_cost_points) total_cost_points, pay_code, mobile,receipt,{$costPriceField}";
         if (!empty($machineIds)) $where[] = ['machine_id','in',$machineIds];
         if ($supplier) unset($where['ao_id']);
@@ -134,7 +134,7 @@ class SaleOrders extends Common
         }
 
         $costPriceField = $hasCostPriceAuth ? "sod.cost_price" : "0 cost_price";
-        $field = "so.machine_id,so.machine_name,so.trade_no,so.mch_no,so.transaction_video,so.order_type,so.pay_type,so.pay_method,so.pay_channel,so.pay_channel_name,so.pay_time,so.out_time,so.create_time,so.out_status,so.refund_status,so.factory,so.inventory_location,so.run_mode,(CASE so.run_mode WHEN 2 THEN '测试模式' ELSE '生产模式' END) run_mode_desc,
+        $field = "so.machine_id,so.machine_name,so.trade_no,so.mch_no,so.transaction_video,so.order_type,so.pay_type,so.pay_method,so.pay_time,so.out_time,so.create_time,so.out_status,so.refund_status,so.factory,so.inventory_location,so.run_mode,(CASE so.run_mode WHEN 2 THEN '测试模式' ELSE '生产模式' END) run_mode_desc,
         sod.sku,sod.g_name,sod.channel_code,sod.retail_price,sod.discount_price,(sod.total_sod_price - sod.refund_amount) total_sod_price,(sod.total_sod_points - sod.refund_points) total_sod_points,(sod.total_sod_cost_points - sod.refund_cost_points) total_sod_cost_points,
         (sod.success_quantity) success_quantity,(sod.fail_quantity) fail_quantity,sod.deliver_pics,(sod.quantity) quantity,sod.refund_quantity,sod.refund_amount,(SELECT organization_name FROM auth_organization ao WHERE ao.ao_id = sod.sod_ao_id) organization_name,{$costPriceField}";
         // if ($postData['supplier']) unset($where['ao_id']);                                                                                                                                                                                                                                                                                                                                                                                                           
@@ -333,7 +333,7 @@ class SaleOrders extends Common
             unset($postData['machine_group_id']);
             if (!$machineIds) return $this->app->machine->rNoData();
         }
-        $where = $this->getWhere($postData,false,["order_id" => "in",'trade_no' => "like","order_type" => "in","mch_no" => "like","machine_name" => "like","machine_id" => "like","pay_type" => "in","pay_channel" => "in",'factory'=>'in','inventory_location'=>'in','out_status'=>'in','run_mode'=>'in']);
+        $where = $this->getWhere($postData,false,["order_id" => "in",'trade_no' => "like","order_type" => "in","mch_no" => "like","machine_name" => "like","machine_id" => "like","pay_type" => "in",'factory'=>'in','inventory_location'=>'in','out_status'=>'in','run_mode'=>'in']);
         $authMch = $this->authMchCannel();
         if ($authMch['status'] != 0) {
             $orderIds = Db::name('sale_orders_details')
