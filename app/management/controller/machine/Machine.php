5613557b897cd390cd92841a7dc26f584cbcb574
18163326 <1924278398@qq.com>
2026-09-04 09:31:37 +0800

    信号等级筛选

--- app/management/controller/machine/Machine.php
+++ app/management/controller/machine/Machine.php
@@ -41,7 +41,9 @@ class Machine extends Common
         $field = $this->field;
         $order = $this->buildMachineListOrder($postData, $field);
         $isOnOff = $postData['is_on_off'] ?? 0;
-        $lowSignal = isset($postData['low_signal']) && $postData['low_signal'] == 1;
+        $signalLevel = isset($postData['low_signal']) && $postData['low_signal'] !== ''
+            ? intval($postData['low_signal'])
+            : null;
         unset($postData['version_sort'],$postData['stock_ratio'],$postData['sort_name'],$postData['sort_order'],$postData['is_on_off'],$postData['low_signal']);
         // 提取 online 参数，单独处理：1=在线(http_online或online为1)，2=离线(http_online和online都为2)
         $onlineValue = null;
@@ -70,9 +72,10 @@ class Machine extends Common
                 $where[] = ['online', '=', 2];
             }
         }
-        if ($lowSignal) {
-            $lowSignalWhere = '(SELECT ssl.rsrp_level FROM sim_signal_log ssl WHERE ssl.m_id = a.m_id ORDER BY ssl.id DESC LIMIT 1) < 2';
-            $where['raw'] = (isset($where['raw']) ? $where['raw'] . ' AND ' : '') . $lowSignalWhere;
+        if ($signalLevel !== null) {
+            $signalValidTime = date('Y-m-d H:i:s', time() - 1800);
+            $signalLevelWhere = "IFNULL((SELECT IF(ssl.created_at >= '{$signalValidTime}', ssl.rsrp_level, 0) FROM sim_signal_log ssl WHERE ssl.m_id = a.m_id ORDER BY ssl.id DESC LIMIT 1), 0) = {$signalLevel}";
+            $where['raw'] = (isset($where['raw']) ? $where['raw'] . ' AND ' : '') . $signalLevelWhere;
         }
         return $this->app->machine->getMList($where,$pageNum,$field,$order);
     }
@@ -178,9 +181,8 @@ class Machine extends Common
         }
 
         if ($sortName == 'rsrp') {
-            $todayStart = date('Y-m-d 00:00:00');
-            $todayEnd = date('Y-m-d 23:59:59');
-            $this->appendSelectField($field, 'rsrp_sort', "(SELECT IFNULL(rsrp, -999) FROM sim_signal_log WHERE m_id = a.m_id AND created_at >= '{$todayStart}' AND created_at <= '{$todayEnd}' ORDER BY id DESC LIMIT 1)");
+            $signalValidTime = date('Y-m-d H:i:s', time() - 1800);
+            $this->appendSelectField($field, 'rsrp_sort', "IFNULL((SELECT IF(created_at >= '{$signalValidTime}', IFNULL(rsrp, -999), -999) FROM sim_signal_log WHERE m_id = a.m_id ORDER BY id DESC LIMIT 1), -999)");
             return 'rsrp_sort';
         }
 
